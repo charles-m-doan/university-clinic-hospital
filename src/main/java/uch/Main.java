efabb8f69c1b3cf618f42f47850a87b5956515ad
charles-m-doan <cmatthewdoan@gmail.com>
2019-05-21 11:26:11 -0400

    Adding initial build with uch package and Main class.

---
+++ src/main/java/uch/Main.java
@@ -0,0 +1,12 @@
+package uch;
+
+public class Main {
+
+	private Main() {
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the University Hospital Clinic");
+	}
+
+}
