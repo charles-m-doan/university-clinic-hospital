27022a6327b5f0ea5b04730a66eb3781f8eb754d
charles-m-doan <cmatthewdoan@gmail.com>
2019-05-28 16:58:10 -0400

    Adding Receptionist class.

--- src/test/java/uch/DoctorTest.java
+++ src/test/java/uch/DoctorTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-public class EmployeeTest {
+public class DoctorTest {
 	
 	Employee underTest = new Doctor("Bill", "Heart");
 
